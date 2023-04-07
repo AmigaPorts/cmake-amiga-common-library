696f93db558efc4b797494d8c59784aa89ac7459
Marlon Beijer <marlon@eevul.org>
2023-04-07 15:34:09 +0200

    Fix some logic

---
+++ setup_amiga_common.cmake
@@ -0,0 +1,11 @@
+# Copyright © 2023 Marlon Beijer <marlon@amigadev.com>
+# https://github.com/mbeijer
+#
+# This Source Code Form is subject to the terms of the
+# Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+# with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
+include(get_verstring)
+include(add_project_info)
+include(add_aminet_readme_generator)
