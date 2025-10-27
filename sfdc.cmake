b86ecaeb0ecd2e7965adfe7edf7f120be312ba81
Marlon Beijer <marlon@eevul.org>
2025-10-27 13:33:02 +0100

    Fix sfdc generation

--- sfdc.cmake
+++ sfdc.cmake
@@ -1,4 +1,4 @@
-# Copyright © 2020 Michal Schulz <michal.schulz@gmx.de>
+# Copyright © 2020 Michal Schulz <michal.schulz@gmx.de>, 2025 Marlon Beijer <marlon@amigadev.com>
 # https://github.com/michalsc
 #
 # This Source Code Form is subject to the terms of the
@@ -6,48 +6,54 @@
 # with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 function(sfdc SFD_FILE)
-    
+
     get_filename_component(LIB_NAME_FULL ${SFD_FILE} NAME_WLE)
     string(REGEX REPLACE "_lib$" "" LIB_NAME ${LIB_NAME_FULL})
 
+    # Create the necessary directories
+    make_directory(${CMAKE_CURRENT_BINARY_DIR}/include/clib)
+    make_directory(${CMAKE_CURRENT_BINARY_DIR}/include/pragmas)
+    make_directory(${CMAKE_CURRENT_BINARY_DIR}/include/proto)
+    make_directory(${CMAKE_CURRENT_BINARY_DIR}/include/inline)
+
     add_custom_command(
-        OUTPUT includes/clib/${LIB_NAME}_protos.h
+        OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/include/clib/${LIB_NAME}_protos.h
         COMMAND sfdc ${CMAKE_CURRENT_SOURCE_DIR}/${SFD_FILE} --mode=clib 
-                     --output=includes/clib/${LIB_NAME}_protos.h
+                     --output=${CMAKE_CURRENT_BINARY_DIR}/include/clib/${LIB_NAME}_protos.h
         DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${SFD_FILE}
         VERBATIM
     )
 
     add_custom_command(
-        OUTPUT includes/pragmas/${LIB_NAME}_pragmas.h
+        OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/include/pragmas/${LIB_NAME}_pragmas.h
         COMMAND sfdc ${CMAKE_CURRENT_SOURCE_DIR}/${SFD_FILE} --mode=pragmas
-                     --output=includes/pragmas/${LIB_NAME}_pragmas.h
+                     --output=${CMAKE_CURRENT_BINARY_DIR}/include/pragmas/${LIB_NAME}_pragmas.h
         DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${SFD_FILE}
         VERBATIM
     )
 
     add_custom_command(
-        OUTPUT includes/proto/${LIB_NAME}.h
+        OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/include/proto/${LIB_NAME}.h
         COMMAND sfdc ${CMAKE_CURRENT_SOURCE_DIR}/${SFD_FILE} --mode=proto
-                     --output=includes/proto/${LIB_NAME}.h
+                     --output=${CMAKE_CURRENT_BINARY_DIR}/include/proto/${LIB_NAME}.h
         DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${SFD_FILE}
         VERBATIM
     )
 
     add_custom_command(
-        OUTPUT includes/inline/${LIB_NAME}.h
+        OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/include/inline/${LIB_NAME}.h
         COMMAND sfdc ${CMAKE_CURRENT_SOURCE_DIR}/${SFD_FILE} --mode=macros
-                     --output=includes/inline/${LIB_NAME}.h
+                     --output=${CMAKE_CURRENT_BINARY_DIR}/include/inline/${LIB_NAME}.h
         DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${SFD_FILE}
         VERBATIM
     )
 
     add_library(${LIB_NAME} INTERFACE
-        includes/clib/${LIB_NAME}_protos.h
-        includes/pragmas/${LIB_NAME}_pragmas.h
-        includes/proto/${LIB_NAME}.h
-        includes/inline/${LIB_NAME}.h
+        ${CMAKE_CURRENT_BINARY_DIR}/include/clib/${LIB_NAME}_protos.h
+        ${CMAKE_CURRENT_BINARY_DIR}/include/pragmas/${LIB_NAME}_pragmas.h
+        ${CMAKE_CURRENT_BINARY_DIR}/include/proto/${LIB_NAME}.h
+        ${CMAKE_CURRENT_BINARY_DIR}/include/inline/${LIB_NAME}.h
     )
-    target_include_directories(${LIB_NAME} INTERFACE ${CMAKE_CURRENT_BINARY_DIR}/includes)
+    target_include_directories(${LIB_NAME} INTERFACE ${CMAKE_CURRENT_BINARY_DIR}/include)
 
 endfunction(sfdc)
