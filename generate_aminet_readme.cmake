bf51be739123796f97313ee3d67f924efc22e0b7
Marlon Beijer <marlon@eevul.org>
2025-10-29 23:29:26 +0100

    Add version to aminet readme

--- generate_aminet_readme.cmake
+++ generate_aminet_readme.cmake
@@ -2,6 +2,7 @@ file(WRITE ${OUTPUT_FILE} "Short:        ${SHORT}\n")
 file(APPEND ${OUTPUT_FILE} "Author:       ${AUTHOR}\n")
 file(APPEND ${OUTPUT_FILE} "Uploader:     ${UPLOADER}\n")
 file(APPEND ${OUTPUT_FILE} "Type:         ${TYPE}\n")
+file(APPEND ${OUTPUT_FILE} "Version:      ${PROJECT_VERSION}\n")
 file(APPEND ${OUTPUT_FILE} "Architecture: ${ARCH}\n")
 file(APPEND ${OUTPUT_FILE} "\n")
 
